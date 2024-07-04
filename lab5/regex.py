8aae1e17e3b87edebeb5a9acb44446871c4014ce
Makaroshka-1 <sarsenbajmiras@gmail.com>
2024-07-04 15:34:27 +0500

    Update regex.py

--- lab5/regex.py
+++ lab5/regex.py
@@ -3,72 +3,48 @@ import re
 with open("row.txt") as file:
   text = file.read()
 
-
-def first_problem():
+def first_problem(text):
   pattern = r'a(b*)'
-  matches = []
-  for i in range( len(text)):
-    if re.fullmatch(pattern, text[i]):
-      matches.append(text[i])
-  return matches
+  return re.findall(pattern, text)
 
-def second_problem():
+def second_problem(text):
   pattern = r'ab{2,3}'
-  matches = []
-  for i in range( len(text)):
-    if re.fullmatch(pattern, text[i]):
-      matches.append(text[i])
-  return matches
+  return re.findall(pattern, text)
 
-def third_problem():
+def third_problem(text):
   pattern = r'[a-z]+_[a-z]+'
-  matches = []
-  for i in range( len(text)):
-    if re.fullmatch(pattern, text[i]):
-      matches.append(text[i])
-  return matches
+  return re.findall(pattern, text)
 
-def fourth_problem():
-  pattern = r'[A-Z]+_[a-z]+'
-  matches = []
-  for i in range( len(text)):
-    if re.fullmatch(pattern, text[i]):
-      matches.append(text[i])
-  return matches
+def fourth_problem(text):
+  pattern = r'[A-Z][a-z]+'
+  return re.findall(pattern, text)
 
-def fifth_problem():
-  pattern = r'a.*b$'
-  matches = []
-  for i in range(len(text)):
-    if re.fullmatch(pattern, text[i]):
-      matches.append(text[i])
-  return matches
+def fifth_problem(text):
+  pattern = r'a.*?b'
+  return re.findall(pattern, text)
 
 def sixth_problem(text):
-  pattern = r'[,.]'
-  for i in range(len(text)):
-    text[i]=re.sub(pattern,":",text[i])
-  text =''.join(text)
-  return text 
+  pattern = r'[ ,.]'
+  return re.sub(pattern, ':', text)
 
 def seventh_problem(text):
     components = text.split('_')
-    camel_case_string = components[0] + ''.join(x.title() for x in components[1:])
-    return camel_case_string
+    return components[0] + ''.join(x.title() for x in components[1:])
 
 def eighth_problem(text):
-    split_strings = re.findall('[A-Z][^A-Z]*', text)
-    return split_strings
+    pattern = r'[A-Z][a-z]*'
+    return re.findall(pattern, text)
 
 def ninth_problem(text):
-    modified_string = re.sub(r'([a-z])([A-Z])', r'\1 \2', text)
-    return modified_string
+    pattern = r'(?<!^)(?=[A-Z])'
+    return re.split(pattern, text)
 
 def tenth_problem(text):
     snake_case_string = re.sub(r'(?<!^)(?=[A-Z])', '_', text).lower()
     return snake_case_string  
 
-print(ninth_problem(text))
+string1 = "ayoNumber1"
+print((third_problem(text)))
 
 
 
